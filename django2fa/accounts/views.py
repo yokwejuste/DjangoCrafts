91e4061dfa98b958d1edea6bcf43634b7b04122d
Fritz Akam <114803800+Fritz-nvm@users.noreply.github.com>
2025-05-23 09:03:13 +0100

    added the logout url to account.urls (#1)
    * added the logout url to account.urls
    
    * removed pycache and env files
    
    * removed generated files and env variables
    
    * updated the gitignore file to the standared
    
    * removed all __pycache__ files
    
    * removed the stated files
    
    * Delete django2fa/.gitignore
    
    ---------
    
    Co-authored-by: Steve Yonkeu <71908316+yokwejuste@users.noreply.github.com>

--- django2fa/accounts/views.py
+++ django2fa/accounts/views.py
@@ -3,7 +3,6 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from django.http import HttpResponse
 
-
 @login_required
 def logout_view(request):
     logout(request)
@@ -11,3 +10,5 @@ def logout_view(request):
 
 def index(request):
     return HttpResponse("Hello, Click the button to confirm you are human, Hummm!!!")
+
+
